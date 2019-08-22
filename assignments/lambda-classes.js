c6f8e297980aa0d4fc47b61605df041f5ba0957a
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-08-22 14:43:36 -0400

    Finished prototye-refractor.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,26 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(PersonAttributes){
+        this.name = PersonAttributes.name,
+        this.age = PersonAttributes.age,
+        this.location = PersonAttributes.location;
+    };
+    speak(){
+        return `Hello my name is ${this.name} I am from ${this.location}`
+    };
+};
+
+class Instructor extends Person{
+    constructor(InstructorAttributes){
+        super(InstructorAttributes);
+        this.specialty = InstructorAttributes.specialty,
+        this.favLanguage = InstructorAttributes.favLanguage,
+        this.catchPhrase = InstructorAttributes.catchPhrase
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
